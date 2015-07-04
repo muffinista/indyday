84b190e86922e76aa7411d43fb50e97678c0e8b9
Colin Mitchell <colin@muffinlabs.com>
2015-07-04 16:56:08 +0000

    cleanup, debugging

--- IndyDaySpeech.rb
+++ IndyDaySpeech.rb
@@ -8,17 +8,24 @@ require 'chatterbot/dsl'
 # generated on 2015-07-03 14:12:42 -0400
 #
 
-
-#tweets = client.user_timeline(count:200).collect(&:id)
-#client.destroy_status(tweets)
-#exit
-
 data = JSON.parse(File.read("speech.json"))
+
+last_tweet = nil
 
 data.each { |entry|
   puts entry.inspect
   puts entry["line"]
 
-  client.update_with_media entry["line"], File.open(entry["image"]) 
+  opts = {}
+  if last_tweet
+   opts[:in_reply_to_status_id] = last_tweet.id
+   last_tweet = client.update_with_media entry["line"], File.open(entry["image"]), opts
+  else
+   last_tweet = client.update_with_media entry["line"], File.open(entry["image"])
+  end
+
+
+
+
   sleep entry["ticks"].to_i
 }
