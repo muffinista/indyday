13f5c0724ee0779d5509bd4b24f08ab8512c1cfe
Colin Mitchell <muffinista@gmail.com>
2017-07-03 14:53:54 +0000

    Add warning bot

---
+++ IndyDaySpeechWarning.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'chatterbot/dsl'
+
+#
+# this is the script for the twitter bot IndyDaySpeech
+# generated on 2015-07-03 14:12:42 -0400
+#
+
+tweet "Good Morning. I'll give a speech sometime in the morning of July 4th."
+
